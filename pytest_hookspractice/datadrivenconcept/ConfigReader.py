a17fa904bcc7d5d086a4fb2917eedec82f9336af
Tamilarasu143 <tamilarasu143@gmail.com>
2026-06-09 13:51:27 +0530

    updated ignore

--- pytest_hookspractice/datadrivenconcept/ConfigReader.py
+++ pytest_hookspractice/datadrivenconcept/ConfigReader.py
@@ -3,27 +3,31 @@ import configparser
 config = configparser.RawConfigParser()
 
 config_path = "Config.ini"
-
 files_loaded = config.read(config_path)
-print("Loaded config files:", files_loaded)
 
 if not files_loaded:
     raise FileNotFoundError("Config.ini not found")
 
+
 def get_application_url():
     return config.get("common info", "baseURL")
+
 
 def get_browser():
     return config.get("common info", "browser")
 
+
 def get_username():
     return config.get("login data", "username")
+
 
 def get_password():
     return config.get("login data", "password")
 
+
 def get_invalidusername():
     return config.get("login invaliddata", "username")
+
 
 def get_invalidpassword():
     return config.get("login invaliddata", "password")
--- pytest_hookspractice/datadrivenconcept/test_demoblazeLogin.py
+++ pytest_hookspractice/datadrivenconcept/test_demoblazeLogin.py
@@ -21,15 +21,13 @@ def get_driver():
     elif browser == "firefox":
         driver = webdriver.Firefox()
     else:
-        raise Exception("Invalid browser in config.ini")
+        raise Exception("Invalid browser in config")
 
     driver.maximize_window()
     return driver
 
 
-
 def test_login():
-
     driver = get_driver()
     driver.get(get_application_url())
 
@@ -37,36 +35,21 @@ def test_login():
 
     wait.until(EC.element_to_be_clickable((By.ID, "login2"))).click()
 
-    username_box = wait.until(
-        EC.visibility_of_element_located((By.ID, "loginusername"))
-    )
-    username_box.clear()
-    username_box.send_keys(get_username())
+    wait.until(EC.visibility_of_element_located((By.ID, "loginusername"))).send_keys(get_username())
+    wait.until(EC.visibility_of_element_located((By.ID, "loginpassword"))).send_keys(get_password())
 
-    password_box = wait.until(
-        EC.visibility_of_element_located((By.ID, "loginpassword"))
-    )
-    password_box.clear()
-    password_box.send_keys(get_password())
-
-    wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//button[text()='Log in']"))
-    ).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Log in']"))).click()
 
     actual_text = wait.until(
         EC.visibility_of_element_located((By.ID, "nameofuser"))
     ).text
 
-    expected_text = "Welcome arasu"
-
-    assert actual_text == expected_text
+    assert actual_text == "Welcome arasu"
 
     driver.quit()
 
 
-
 def test_invalidlogin():
-
     driver = get_driver()
     driver.get(get_application_url())
 
@@ -74,32 +57,15 @@ def test_invalidlogin():
 
     wait.until(EC.element_to_be_clickable((By.ID, "login2"))).click()
 
-    username_box = wait.until(
-        EC.visibility_of_element_located((By.ID, "loginusername"))
-    )
-    username_box.clear()
-    username_box.send_keys(get_invalidusername())
+    wait.until(EC.visibility_of_element_located((By.ID, "loginusername"))).send_keys(get_invalidusername())
+    wait.until(EC.visibility_of_element_located((By.ID, "loginpassword"))).send_keys(get_invalidpassword())
 
-    password_box = wait.until(
-        EC.visibility_of_element_located((By.ID, "loginpassword"))
-    )
-    password_box.clear()
-    password_box.send_keys(get_invalidpassword())
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Log in']"))).click()
 
-    wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//button[text()='Log in']"))
-    ).click()
-
-    # ---------------------------
-    # Handle alert safely
-    # ---------------------------
     alert = wait.until(EC.alert_is_present())
     alert_text = alert.text
     alert.accept()
 
-    print("Alert message:", alert_text)
-
-    # Optional assertion (recommended)
     assert alert_text is not None
 
     driver.quit()
